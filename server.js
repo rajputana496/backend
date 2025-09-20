adb57d4227f1f1fdfdb478833bdfbb0dd51065ca
Rajpratap25-AI <rajpratapsinghaulakh@gmail.com>
2025-09-20 23:21:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 
 // Import your controllers
 import { signup, login } from "./userController.js";
-import { signupDoctor, loginDoctor } from "./DoctorController.js";
+import { signupDoctor, loginDoctor } from "./doctorController.js";
 import { bookConsultation, getUserConsultations } from "./consultationController.js";
 import { rescheduleConsultation } from "./doctorRescheduleController.js";
 
@@ -175,4 +175,5 @@ const HOST = "0.0.0.0"; // listen on all network interfaces
 app.listen(PORT, HOST, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
+
 
